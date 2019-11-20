907da96a6522613dc55ee02158b95894f0117cd1
moataz <mtzhisham@gmail.com>
2019-11-20 05:01:46 +0200

    Added internet and shared preference utils

--- app/src/main/java/dev/moataz/flickrim/ResponseBody.kt
+++
@@ -1,7 +0,0 @@
-package dev.moataz.flickrim
-
-import com.google.gson.annotations.SerializedName
-
-
-data class ResponseBody(@SerializedName("photos") val photos: Photos? = null,
-                        @SerializedName("stat") val stat: String? = null )
